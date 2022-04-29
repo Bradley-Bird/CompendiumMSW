93cc08a8f6f0476a945c5b15259f08ce4c8faf02
Bradley-Bird <Bradleybird2@icloud.com>
2022-04-29 12:31:55 -0700

    started test server

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,2 +1,15 @@
+import { setupServer } from 'mws/node';
+import { rest } from 'msw';
+import characters from './fixtures/characters';
 global.fetch = (...args) =>
   import('cross-fetch').then(({ default: fetch }) => fetch(...args));
+
+export const server = setupServer(
+  rest.get('https://ghibliapi.herokuapp.com/people', (req, res, ctx) => {
+    res(ctx.json(characters));
+  })
+);
+
+beforeAll(() => server.listen);
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close);
